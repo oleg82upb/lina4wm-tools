0f221eca4d3fcd492e6a2ba411db49d746e9ab92
thhaarhoff <thhaarhoff@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2013-01-23 08:40:56 +0000

    Ast-Export now providing XReferences

--- llvm-parser/plugins/de.upb.llvm_parser.ui/src/de/upb/llvm_parser/ui/contentassist/LLVMASTAssistant.java
+++ llvm-parser/plugins/de.upb.llvm_parser.ui/src/de/upb/llvm_parser/ui/contentassist/LLVMASTAssistant.java
@@ -1,9 +1,14 @@
 package de.upb.llvm_parser.ui.contentassist;
 
 import java.io.IOException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.eclipse.core.filesystem.EFS;
@@ -18,7 +23,10 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.mwe.utils.StandaloneSetup;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreePath;
@@ -36,10 +44,19 @@ import org.eclipse.ui.dialogs.FileSelectionDialog;
 import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.xtext.builder.resourceloader.IResourceLoader;
+import org.eclipse.xtext.linking.lazy.LazyLinkingResource;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.parser.IParser;
+import org.eclipse.xtext.resource.ClasspathUriResolutionException;
+import org.eclipse.xtext.resource.ClasspathUriUtil;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
 
+import com.google.inject.Injector;
+
+import de.upb.llvm_parser.LLVMStandaloneSetup;
 import de.upb.llvm_parser.parser.antlr.LLVMParser;
 import de.upb.llvm_parser.ui.LLVMUtil;
 import de.upb.llvm_parser.ui.internal.LLVMActivator;
@@ -48,9 +65,8 @@ import de.upb.llvm_parser.ui.internal.LLVMActivator;
  * @author Thomas Haarhoff (thha@mail.upb.de)
  * 
  */
-public class LLVMASTAssistant extends LLVMParser implements
+public class LLVMASTAssistant extends XtextResource implements
 		IObjectActionDelegate {
-
 	public LLVMASTAssistant() {
 		super();
 	}
@@ -79,9 +95,31 @@ public class LLVMASTAssistant extends LLVMParser implements
 
 				IDE.openEditorOnFileStore(page, EFS.getLocalFileSystem()
 						.getStore(ipath));
-				saveAstUtil((IFile) last, window);
 			}
-
+			if(testDocument((IFile)last, window))
+			{
+					MessageBox messageBox = new MessageBox(window.getShell(),
+							SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+					messageBox
+							.setMessage("There are errors located on selected file.\n"
+									+ "Do really want to save the AST-file?");
+					messageBox.setText("Syntax Errors");
+					int response = messageBox.open();
+					if (response == SWT.NO) {
+						System.out
+								.println("Save AST abort, because of file containing errors");
+						return;
+					}
+			}
+			XtextResourceSet xtextResourceSet = new XtextResourceSet();
+			URI uri = URI.createURI(((IFile)last).getFullPath().toString());
+			Resource resource = xtextResourceSet.getResource(uri, true);
+			EObject ast = resource.getContents().get(0);
+			ResourceSet resSet = new ResourceSetImpl();
+			resource = resSet.createResource(URI.createFileURI(getLocation((IFile) last, window)));
+			resource.getContents().add(ast);
+			resource.save(Collections.EMPTY_MAP);
+			refreshWorkspace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -93,53 +131,15 @@ public class LLVMASTAssistant extends LLVMParser implements
 	 * @param window
 	 * Saves the parsed Model of llvmFile to a serialized llvm-File containing the AST of the Model. 
 	 */
-	public void saveAstUtil(IFile llvmFile, IWorkbenchWindow window) {
+	private String getLocation(IFile llvmFile, IWorkbenchWindow window) {
 		/* Dialog for target file location */
 		SaveAsDialog sad = new SaveAsDialog(window.getShell());
 		sad.setOriginalName(llvmFile.getName()+".llvm");
 		sad.open();
 		if(sad.getResult()==null)
-			return;
+			return null;
 		String location = sad.getResult().toOSString();
-
-		/* Prepare file for parser */
-		String content;
-		try {
-			content = convertStreamToString((llvmFile).getContents());
-
-			StringReader reader = new StringReader(content);
-
-			/* Parser invokation */
-			IParser parser = LLVMUtil.getLLVMParser();
-			IParseResult ip = parser.parse(reader);
-			if (ip.getSyntaxErrors().iterator().hasNext()) {
-				MessageBox messageBox = new MessageBox(window.getShell(),
-						SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-				messageBox
-						.setMessage("There are errors located on selected file.\n"
-								+ "Do really want to save the AST-file?");
-				messageBox.setText("Syntax Errors");
-				int response = messageBox.open();
-				if (response == SWT.NO) {
-					System.out
-							.println("Save AST abort, because of file containing errors");
-					return;
-				}
-
-			}
-
-			/* Save the AST here */
-			EObject ast = ip.getRootASTElement();
-			ResourceSet resSet = new ResourceSetImpl();
-			Resource resource = resSet.createResource(URI
-					.createFileURI(location));
-			resource.getContents().add(ast);
-			resource.save(Collections.EMPTY_MAP);
-			refreshWorkspace();
-		} catch (CoreException | IOException e) {
-			System.out.println("Error on converting File-content to String");
-			e.printStackTrace();
-		}
+			return location;
 	}
 
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
@@ -147,7 +147,19 @@ public class LLVMASTAssistant extends LLVMParser implements
 
 	public void selectionChanged(IAction action, ISelection selection) {
 	}
-
+	
+	private boolean testDocument(IFile llvmFile, IWorkbenchWindow window) throws CoreException {
+		/* Prepare file for parser */
+		String content;
+			content = convertStreamToString((llvmFile).getContents());
+			StringReader reader = new StringReader(content);
+			/* Parser invokation */
+			IParser parser = LLVMUtil.getLLVMParser();
+			IParseResult ip = parser.parse(reader);
+			return (ip.getSyntaxErrors().iterator().hasNext());
+	}
+	
+	
 	public static String convertStreamToString(InputStream is) {
 		String st = "";
 		Scanner s = new Scanner(is).useDelimiter("\\A");
