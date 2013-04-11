fbe7d128ae5210570f382e3fb8dc68541a46fcdb
oleg82@zitmail.uni-paderborn.de <oleg82@zitmail.uni-paderborn.de@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2013-04-11 08:16:01 +0000

    - changed control flow layout to top-down instead of bottom up

---
+++ controlflow/plugins/de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/providers/ControlFlowLayoutProvider.java
@@ -0,0 +1,43 @@
+package de.upb.lina.cfg.controlflow.diagram.providers;
+
+import java.util.Iterator;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.providers.TopDownProvider;
+import org.eclipse.gmf.runtime.diagram.ui.services.layout.ILayoutNode;
+import org.eclipse.gmf.runtime.diagram.ui.services.layout.ILayoutNodeOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.layout.LayoutType;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
+
+public class ControlFlowLayoutProvider extends TopDownProvider {
+
+	public static String DEFAULT_LAYOUT = "Default";
+	public static String CFG = "ControlFlow";
+
+	public ControlFlowLayoutProvider() {
+		super();
+	}
+
+	public boolean provides(IOperation operation) {
+		// enable this provider only on control flow diagrams
+		if (operation instanceof ILayoutNodeOperation) {
+			Iterator nodes = ((ILayoutNodeOperation) operation)
+					.getLayoutNodes().listIterator();
+			if (nodes.hasNext()) {
+				View node = ((ILayoutNode) nodes.next()).getNode();
+				Diagram container = node.getDiagram();
+				if (container == null
+						|| !(container.getType().equals(CFG))) //$NON-NLS-1$
+					return false;
+			}
+		} else {
+			return false;
+		}
+		IAdaptable layoutHint = ((ILayoutNodeOperation) operation)
+				.getLayoutHint();
+		String layoutType = (String) layoutHint.getAdapter(String.class);
+		return LayoutType.DEFAULT.equals(layoutType);
+	}
+}
