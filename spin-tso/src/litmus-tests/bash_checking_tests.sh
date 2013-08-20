1eadcb92708c8013d1183c1ba9ba982320eddb90
muetze.annika@gmail.com <muetze.annika@gmail.com@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2013-08-20 13:02:06 +0000

    bash-skript

---
+++ spin-tso/src/litmus-tests/bash_checking_tests.sh
@@ -0,0 +1,70 @@
+# Bashskript für automatisiertes Testen von Promeladateien mit ispin :-)
+# Benötigter Aufbau der Testdatei: ausgabedatei.txt;zuTestendeDatei.pml;LTLFormel1;LTLFormel2;... 
+# author: Annika Mütze muetze.annika@gmail.com
+# date: May 2013
+
+## Komplieren von bash-dateien: chmod +x pfad/zu/mein_Skript 
+## ausführen: ./skriptname 
+
+#!/bin/bash
+
+cd workspace/projects/src/litmus-tests;
+
+# Demonstriert den Befehl read zum zeilenweisen Lesen einer Datei
+if [ $# -lt 1 ]
+then
+   echo "usage: $0 datei_zum_lesen"
+   exit 1
+fi
+#das Trennungszeichen ist nun ein ";"
+BACKIFS="$IFS"
+IFS=\;
+counter=1
+# Argument $1 (Eingabe beim Aufruf des Skripts) soll zeilenweise eingelesen werden
+while read dateiinhalt
+do
+	set $dateiinhalt
+	for daten in "$@"
+	do
+		case "$counter" in
+			1) echo "ausgabedatei: $daten"
+				#Datei, in die Ausgabe gespeichert wird
+   			ausgabedatei=$daten
+   			counter=`expr $counter + 1`
+			;;
+			2) echo "datei: $daten"
+				#Name der zu überprüfenden Datei
+				dateiname=$daten
+				counter=`expr $counter + 1`
+			
+				spin -a $dateiname;
+				gcc -DMEMLIM=1024 -O2 -DXUSAFE -w -o pan pan.c;
+	
+				if  !( [ -f "./$ausgabedatei" ] ) #wenn Datei exsistiert liefert [ -f "./$ausgabedatei" ] Wert ungleich null ("true") -> wegen Negation geht Skript dann in else Teil. Wenn Datei nicht existiert wird sie angelegt mit touch.
+				then
+					touch $ausgabedatei
+				else 
+					echo "Datei existiert bereits. Ändern Sie bitte den Namen der Ausgabedatei."
+					exit
+				fi
+				echo "Test: $dateiname" >>$ausgabedatei
+				echo "--------------------------------------------------------------------------------------------------------------" >>$ausgabedatei
+				;;
+			
+			3) echo "Test: $daten"
+				testname=$daten
+								
+				echo "---------$testname----------------------------------------------------------------------------------------------" >>$ausgabedatei
+				echo "--------------------------------------------------------------------------------------------------------------" >>$ausgabedatei
+				
+				./pan -m10000  -a -n -N $testname >> $ausgabedatei 
+				echo "--------------------------------------------------------------------------------------------------------------" >>$ausgabedatei
+			;;
+		esac
+	   
+	done
+	counter=1
+
+done < $1
+IFS=$BACKIFS
+
