9c3f3b045df5747ee77661b06cb7304723acb0ec
oleg82@zitmail.uni-paderborn.de <oleg82@zitmail.uni-paderborn.de@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2014-08-19 11:10:21 +0000

    - marking edges by color if they are interacting with the memory

--- controlflow/plugins/de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/edit/parts/CustomTransitionEditPart.java
+++ controlflow/plugins/de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/edit/parts/CustomTransitionEditPart.java
@@ -3,6 +3,7 @@
  */
 package de.upb.lina.cfg.controlflow.diagram.edit.parts;
 
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Connection;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.Connector;
@@ -14,43 +15,67 @@ import de.upb.lina.cfg.controlflow.Transition;
  * @author Oleg Travkin
  * 
  */
-public class CustomTransitionEditPart extends TransitionEditPart {
+public class CustomTransitionEditPart extends TransitionEditPart
+{
 	private CustomLabelingUtil labelutil = new CustomLabelingUtil();
 	private static String EMPTY = "null";
 
 	/**
 	 * @param view
 	 */
-	public CustomTransitionEditPart(View view) {
+	public CustomTransitionEditPart(View view)
+	{
 		super(view);
 	}
 
 	@Override
-	protected Connection createConnectionFigure() {
+	protected Connection createConnectionFigure()
+	{
 		TransitionFigure figure = new TransitionFigure();
 
 		String labelText = null;
-		if (this.getModel() != null) {
+		if (this.getModel() != null)
+		{
 			Transition t = null;
-			if (this.getModel() instanceof Connector) {
+			if (this.getModel() instanceof Connector)
+			{
 				Connector c = (Connector) this.getModel();
 				EObject o = c.getElement();
-				if (o instanceof Transition) {
+				if (o instanceof Transition)
+				{
 					t = (Transition) o;
 				}
 			}
 			labelText = getTransitionLabelStringFor(t);
-		} else {
+		} else
+		{
 			labelText = EMPTY;
 		}
 		figure.getTransitionLabel().setText(labelText);
+		figure.setForegroundColor(ColorConstants.darkGreen);
+		figure.setBackgroundColor(ColorConstants.darkGreen);
 		return figure;
 	}
 
-	protected String getTransitionLabelStringFor(Transition t) {
-		if (t == null) {
+	protected String getTransitionLabelStringFor(Transition t)
+	{
+		if (t == null)
+		{
 			return EMPTY;
 		}
 		return labelutil.getNewTransitionLabel(t);
+	}
+
+	@Override
+	protected void refreshVisuals()
+	{
+		refreshLineWidth();
+		super.refreshVisuals();
+	}
+
+	@Override
+	protected void setLineWidth(int width)
+	{
+		((TransitionFigure) getFigure()).setLineWidth(width);
 	}
 }
---
+++ controlflow/plugins/de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/providers/CustomControlFlowViewProvider.java
@@ -0,0 +1,69 @@
+package de.upb.lina.cfg.controlflow.diagram.providers;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.notation.Edge;
+import org.eclipse.gmf.runtime.notation.LineStyle;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.swt.graphics.RGB;
+
+import de.upb.lina.cfg.controlflow.Transition;
+import de.upb.llvm_parser.llvm.Alloc;
+import de.upb.llvm_parser.llvm.CmpXchg;
+import de.upb.llvm_parser.llvm.Fence;
+import de.upb.llvm_parser.llvm.Load;
+import de.upb.llvm_parser.llvm.Store;
+
+public class CustomControlFlowViewProvider extends ControlFlowViewProvider
+{
+	private final static RGB READ = new RGB(0, 200, 0);
+	private final static RGB WRITE = new RGB(200, 0, 0);
+	private final static RGB CAS = new RGB(0, 200, 200);
+	private final static RGB FENCE = new RGB(0, 0, 200);
+	private final static RGB ALLOCA = new RGB(200, 200, 0);
+
+	/**
+	 * @generated
+	 */
+	public Edge createTransition_4001(EObject domainElement, View containerView, int index, boolean persisted,
+			PreferencesHint preferencesHint)
+	{
+		Edge edge = super.createTransition_4001(domainElement, containerView, index, persisted, preferencesHint);
+		LineStyle style = (LineStyle) edge.getStyle(NotationPackage.Literals.LINE_STYLE);
+
+		if (style == null)
+		{
+			return edge;
+		}
+
+		EObject transitionElement = ((Transition) edge.getElement()).getInstruction();
+
+		int color = style.getLineColor();
+		if (transitionElement instanceof Store)
+		{
+			color = WRITE.hashCode();
+		} else if (transitionElement instanceof Load)
+		{
+			color = READ.hashCode();
+		} else if (transitionElement instanceof CmpXchg)
+		{
+			color = CAS.hashCode();
+		} else if (transitionElement instanceof Alloc)
+		{
+			color = ALLOCA.hashCode();
+		} else if (transitionElement instanceof Fence)
+		{
+			color = FENCE.hashCode();
+		}
+
+		if (color != style.getLineColor())
+		{
+			style.setLineWidth(5);
+			style.setLineColor(color);
+		}
+
+		return edge;
+	}
+
+}
