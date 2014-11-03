e5fdb2ad40fd01b5a4af38ae9cb81e1845a8f47b
alexander.hetzer@gmail.com <alexander.hetzer@gmail.com@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2014-11-03 12:10:22 +0000

    -added testsuite + another test

---
+++ controlflow/plugins/de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/tests/RU_T_IndWR.java
@@ -0,0 +1,56 @@
+package de.upb.lina.cfg.tools.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.upb.lina.cfg.controlflow.ControlFlowDiagram;
+import de.upb.lina.cfg.controlflow.ControlFlowLocation;
+import de.upb.lina.cfg.tools.ReorderingUtil;
+import de.upb.llvm_parser.llvm.FunctionDefinition;
+
+public class RU_T_IndWR extends TSO_Test {
+
+	@Before
+	public void setUp() throws Exception {
+		astLoc = "testdata/Test_Independent_Write_Read.s.llvm";
+		super.setUp();
+	}
+
+	@Test
+	public final void testCreateReachibilityGraph() {
+		ReorderingUtil util = new ReorderingUtil();
+
+		ControlFlowDiagram diag = util.createReachibilityGraph((FunctionDefinition) ast.getElements().get(0));
+		
+		//check for correct amount of locations and edges
+		assertEquals(diag.getLocations().size(),9);
+		assertEquals(diag.getTransitions().size(),10);
+		
+		List<ControlFlowLocation> locs = diag.getLocations();
+		
+		List<ControlFlowLocation> nonEmptyBuffers  = new ArrayList<ControlFlowLocation>();
+		for(ControlFlowLocation l: locs){
+			if(!l.getBuffer().getAddressValuePairs().isEmpty()){
+				nonEmptyBuffers.add(l);
+			}
+		}
+		
+		//check that there is only three nodes with a buffer
+		assertEquals(nonEmptyBuffers.size(), 3);
+		
+		//check that all buffers contain the correct elements
+		for(ControlFlowLocation l: nonEmptyBuffers){
+			String buffer = gUtil.getBufferAsString(l);
+			boolean isValidBuffer = buffer.equals(l.getPc()+"<(%b,null)>");
+			assertTrue(isValidBuffer);
+			
+		}
+	}
+
+}
---
+++ controlflow/plugins/de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/tests/RunAllTests.java
@@ -0,0 +1,11 @@
+package de.upb.lina.cfg.tools.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RU_T_DepWR.class, RU_T_IndWR.class })
+public class RunAllTests {
+
+}
