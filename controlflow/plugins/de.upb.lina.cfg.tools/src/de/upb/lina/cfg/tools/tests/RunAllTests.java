ef20f1c345d2534be7be024831fe743b5edc90fe
alexander.hetzer@gmail.com <alexander.hetzer@gmail.com@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2015-01-04 09:24:31 +0000

    - added rest of the normal tests, continuing with loop tests and reverse tests

--- controlflow/plugins/de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/tests/RunAllTests.java
+++ controlflow/plugins/de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/tests/RunAllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RU_T_DepWR.class, RU_T_IndWR.class, RU_T_DepWR_1.class,  RU_T_DepWR_1_1.class, RU_T_DepWR_12.class, RU_T_DepWR_2.class , RU_T_DepWR_2_1.class, RU_T_IndWR_1.class})
+@SuiteClasses({ RU_T_DepWR.class, RU_T_IndWR.class, RU_T_DepWR_1.class,  RU_T_DepWR_1_1.class, RU_T_DepWR_12.class, RU_T_DepWR_2.class , RU_T_DepWR_2_1.class, RU_T_IndWR_1.class, RU_T_IndWR_1_1.class, RU_T_IndWR_2_1.class, RU_T_IndWR_2.class})
 public class RunAllTests {
 
 }
