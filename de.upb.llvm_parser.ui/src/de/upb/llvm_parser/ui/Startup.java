191cf9f783f0c5a45f944f76b53283e8af77b7ba
oleg82upb <oleg82@upb.de>
2015-04-08 17:01:51 +0200

    - added startup extension registering the llvm package (non-deterministically it doesn't work anyway)

---
+++ de.upb.llvm_parser.ui/src/de/upb/llvm_parser/ui/Startup.java
@@ -0,0 +1,15 @@
+package de.upb.llvm_parser.ui;
+
+import org.eclipse.ui.IStartup;
+
+import de.upb.llvm_parser.llvm.LlvmPackage;
+
+public class Startup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		//just make sure the package is registered on startup
+		LlvmPackage.eINSTANCE.getLLVM();
+	}
+
+}
