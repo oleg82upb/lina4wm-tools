97773c2d49aa6ec541c9967070b4e04aecfdc80c
Alexander Hetzer <alexander.hetzer@gmail.com>
2015-05-17 14:18:18 +0200

    - added KivTransformationOperation in order to be able to add the kiv-packge to the exported packages (to resolve conflicts caused by accceleo) - added Acceleo nature to the project in order to make Acceleo work properly in the merged project - added Acceleo builder to the project in order to make Acceleo work properly

---
+++ de.upb.lina.transformations/src/de/upb/lina/transformations/kiv/KivTransformationOperation.java
@@ -0,0 +1,13 @@
+package de.upb.lina.transformations.kiv;
+
+import de.upb.lina.transformations.wizards.TransformationOperation;
+
+public class KivTransformationOperation extends TransformationOperation {
+
+	public KivTransformationOperation(String graphModelFileLocation,
+			String targetContainer, String targetName, String fileEnding) {
+		super(graphModelFileLocation, targetContainer, targetName, fileEnding);
+		// TODO Auto-generated constructor stub
+	}
+
+}
