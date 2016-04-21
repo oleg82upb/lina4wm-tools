ef3f07a8d5b520de6d8dd393acce3e4eb797df29
Alexander Hetzer <alexander.hetzer@gmail.com>
2016-04-21 21:04:51 +0200

    Moved common wizard messages to WizardMessageConstants class.

---
+++ de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/wizards/WizardMessageConstants.java
@@ -0,0 +1,16 @@
+package de.upb.lina.cfg.tools.wizards;
+
+public class WizardMessageConstants {
+
+	//Label Constants
+	public static final String MSG_STATUS_INVALID_OUTPUT_CONTAINER_NAME = "The folder path has to start with '/'.";
+	public static final String MSG_STATUS_INVALID_OUTPUT_CONTAINER = "Invalid output folder.";
+	public static final String MSG_STATUS_INVALID_OUTPUT_FILE_NAME = "Invalid output filename.";
+	public static final String MSG_STATUS_OK = "Check your input and press next or finish!";
+	public static final String MSG_STATUS_INPUT_FILE_CANNOT_BE_LOADED = "Unable to load the specified input file.";
+	public static final String MSG_STATUS_INPUT_FILE_NOT_EXISTING = "The specified input file does not exist.";
+	/*GUI button labels*/
+	public static final String BT_TX_BROWSE = "Browse";
+
+
+}
