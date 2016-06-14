afc2d55d5ff61a7738a16de5dd03de25c93a1bc7
Alexander Hetzer <alexander.hetzer@gmail.com>
2016-06-14 13:00:11 +0200

    Extracted string constants from GraphUtility into StringUtils.

---
+++ de.upb.lina.cfg.tools/src/de/upb/lina/cfg/tools/StringUtils.java
@@ -0,0 +1,23 @@
+package de.upb.lina.cfg.tools;
+
+
+/**
+ * This class is a static utility class providing convenience methods and constants which are useful
+ * when working with strings.
+ * 
+ * @author Alexander Hetzer
+ *
+ */
+public class StringUtils {
+
+   public static final String RIGHT_BRACKET = ")";
+   public static final String LEFT_BRACKET = "(";
+   public static final String EMPTY_STRING = "";
+   public static final String NUMBERS_REGEX = "[0-9]";
+   public static final String REPLACE_DOTS_REGEX = "\\.";
+
+
+   private StringUtils() {
+   }
+
+}
