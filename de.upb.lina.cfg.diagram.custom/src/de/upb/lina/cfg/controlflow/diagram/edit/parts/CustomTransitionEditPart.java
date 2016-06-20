65a32dfcdc99ead8f6695fd5757202df35278c15
Alexander Hetzer <alexander.hetzer@gmail.com>
2016-06-20 11:52:53 +0200

    Adapted CustomTransitionEditPart.

--- de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/edit/parts/CustomTransitionEditPart.java
+++ de.upb.lina.cfg.diagram.custom/src/de/upb/lina/cfg/controlflow/diagram/edit/parts/CustomTransitionEditPart.java
@@ -9,7 +9,7 @@ import org.eclipse.gmf.runtime.notation.Connector;
 import org.eclipse.gmf.runtime.notation.View;
 
 import de.upb.lina.cfg.controlflow.Transition;
-import de.upb.lina.cfg.tools.TransitionToStringConverter;
+import de.upb.lina.cfg.tools.stringrepresentation.TransitionToStringConverter;
 
 /**
  * @author Oleg Travkin
