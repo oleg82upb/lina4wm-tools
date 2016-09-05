4629e2dcb2501cb3f124532cd729d02ac663620a
Alexander Hetzer <alexander.hetzer@gmail.com>
2016-09-05 12:35:26 +0200

    Improved code of TransformationOperation.

--- de.upb.lina.transformations/src/de/upb/lina/transformations/logic/TransformationOperation.java
+++ de.upb.lina.transformations/src/de/upb/lina/transformations/logic/TransformationOperation.java
@@ -33,7 +33,9 @@ import de.upb.lina.transformations.Activator;
  *
  */
 public class TransformationOperation extends WorkspaceModifyOperation {
+
    protected String inputFileLocation;
+
    protected String outputFolderLocation;
    protected String outputFileName;
    protected String outputFileExtension;
@@ -41,6 +43,10 @@ public class TransformationOperation extends WorkspaceModifyOperation {
 
    protected GeneratorData generatorData;
    protected TransformationConfiguration configuration;
+
+   private ETransformationType transformationType;
+
+   private Profiler profiler;
 
 
    public TransformationOperation(String outputFolderLocation, String outputFileName, String outputFileExtension,
@@ -56,88 +62,194 @@ public class TransformationOperation extends WorkspaceModifyOperation {
 	protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
 			InterruptedException
 	{
-		long tStart = System.currentTimeMillis();
-		// Run Precomputation
-		GendataPrecomputer precomp = new GendataPrecomputer(configuration);
-		generatorData = precomp.computeGeneratorData();
+		long startTimeInMillis = System.currentTimeMillis();
 
-		// get workspace root
-		IWorkspaceRoot workSpaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+      computeGenerationData();
+      computeCompleteOutputPath();
 
-		// get target path within project
-		IPath targetFolderCont = new Path(outputFolderLocation);
-		IResource resource = null;
-		if (targetFolderCont.isAbsolute())
-		{
-			resource = workSpaceRoot.findMember(targetFolderCont);
-		} else
-		{
-			resource = workSpaceRoot.findMember(targetFolderCont.makeAbsolute());
-		}
-
-		// build correct full path
-		completeOutputPath = resource.getLocation();
-		Profiler profiler = new Profiler();
+      profiler = new Profiler();
 		try
 		{
-
-			if (CFGConstants.IN_DEBUG_MODE)
-			{
-				AcceleoEngineUtils.setProfiler(profiler);
-				AcceleoPreferences.switchQueryCache(false);
-				AcceleoPreferences.switchDebugMessages(true);
-				AcceleoPreferences.switchProfiler(true);
-				AcceleoPreferences.switchSuccessNotifications(false);
-			} else
-			{
-				AcceleoPreferences.switchDebugMessages(false);
-				AcceleoPreferences.switchProfiler(false);
-				AcceleoPreferences.switchSuccessNotifications(false);
-			}
-
-			ETransformationType transformationType = this.configuration.getTransformationType();
-			if (transformationType == null)
-			{
-				Activator.logError(
-						"Unknown transformation type " + this.configuration.getTransformationType(),
-						new RuntimeException("Unknown transformation type "
-								+ this.configuration.getTransformationType()));
-			}
-			ArrayList<Object> args = new ArrayList<Object>();
-
-			if (transformationType == ETransformationType.PROMELA
-					|| transformationType == ETransformationType.PROMELA_OPERATIONAL)
-			{
-				args.add(outputFileName + outputFileExtension);
-			}
-
-			transformationType.createAcceleoGenerator(generatorData, completeOutputPath.toFile(), args);
-			AbstractAcceleoGenerator generator = transformationType.createAcceleoGenerator(generatorData,
-					completeOutputPath.toFile(), args);
-			generator.doGenerate(new BasicMonitor());
-
+			setAcceleoPreferences();
+         determineTransformationType();
+         startTransformation();
 		} catch (IOException e)
 		{
 			Activator.logError(e.getMessage(), e);
 		} finally
 		{
-			if (CFGConstants.IN_DEBUG_MODE)
-			{
-				try
-				{
-					AcceleoPreferences.switchProfiler(false);
-					String profileURI = completeOutputPath.toOSString() + "\\lastprofile.mtlp";
-					profiler.save(profileURI);
-				} catch (Exception e2)
-				{
-					// do nothing
-				}
-			}
+         saveAndResetProfiler();
 		}
 
 		ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, monitor);
-		long tEnd = System.currentTimeMillis();
-		Activator.log(IStatus.INFO, "Operation completed in " + (tEnd - tStart) + "ms.", null);
+		long endTimeInMillis = System.currentTimeMillis();
+		Activator.log(IStatus.INFO, "Operation completed in " + (endTimeInMillis - startTimeInMillis) + "ms.", null);
 
 	}
+
+
+   /**
+    * Starts the transformation.
+    * 
+    * @throws IOException if the output files cannot be generated correctly during the
+    *            transformation
+    */
+   private void startTransformation() throws IOException
+   {
+      AbstractAcceleoGenerator generator = createAcceleoGenerator();
+      generator.doGenerate(new BasicMonitor());
+   }
+
+
+   /**
+    * Saves and resets the profiler.
+    */
+   private void saveAndResetProfiler()
+   {
+      if (CFGConstants.IN_DEBUG_MODE)
+      {
+         try
+         {
+            AcceleoPreferences.switchProfiler(false);
+            String profileURI = completeOutputPath.toOSString() + "\\lastprofile.mtlp";
+            profiler.save(profileURI);
+         } catch (Exception e2)
+         {
+            // do nothing
+         }
+      }
+   }
+
+
+   /**
+    * Determines the transformation type.
+    */
+   private void determineTransformationType()
+   {
+      transformationType = this.configuration.getTransformationType();
+      assertTransformationTypeNotNull();
+   }
+
+
+   /**
+    * Creates the {@link AbstractAcceleoGenerator} fitting the local transformation type.
+    * 
+    * @return the Acceleo generator fitting the local transformation type
+    * @throws IOException if a problem during the creation of the Acceleo generator occurs
+    */
+   private AbstractAcceleoGenerator createAcceleoGenerator() throws IOException
+   {
+      ArrayList<Object> transformationArguments = generateTransformationArguments();
+
+      AbstractAcceleoGenerator generator = transformationType.createAcceleoGenerator(generatorData,
+ completeOutputPath.toFile(),
+            transformationArguments);
+      return generator;
+   }
+
+
+   /**
+    * Generates and returns the transformation arguments required by the Acceleo generator.
+    * 
+    * @return the transformation arguments required by the Acceleo generator
+    */
+   private ArrayList<Object> generateTransformationArguments()
+   {
+      ArrayList<Object> transformationArguments = new ArrayList<Object>();
+
+      if (transformationType == ETransformationType.PROMELA
+      		|| transformationType == ETransformationType.PROMELA_OPERATIONAL)
+      {
+         transformationArguments.add(outputFileName + outputFileExtension);
+      }
+      return transformationArguments;
+   }
+
+
+   /**
+    * Computes the complete output path.
+    */
+   private void computeCompleteOutputPath()
+   {
+      IResource outputResource = findOutputResource();
+      completeOutputPath = outputResource.getLocation();
+   }
+
+
+   /**
+    * Computes the generation data required as additional information to the store buffer graph
+    * model.
+    */
+   private void computeGenerationData()
+   {
+      if (generatorData == null)
+      {
+         GendataPrecomputer precomp = new GendataPrecomputer(configuration);
+         generatorData = precomp.computeGeneratorData();
+      }
+   }
+
+
+   /**
+    * Asserts that the local transformation type is not {@code null} and throws a
+    * {@link RuntimeException} otherwise.
+    */
+   private void assertTransformationTypeNotNull()
+   {
+      if (transformationType == null)
+      {
+         RuntimeException runtimeException = new RuntimeException("Unknown transformation type "
+               + this.configuration.getTransformationType());
+      	Activator.logError(
+      			"Unknown transformation type " + this.configuration.getTransformationType(),
+ runtimeException);
+         throw runtimeException;
+      }
+   }
+
+
+   /**
+    * Sets the Acceleo preferences.
+    */
+   private void setAcceleoPreferences()
+   {
+      if (CFGConstants.IN_DEBUG_MODE)
+      {
+      	AcceleoEngineUtils.setProfiler(profiler);
+      	AcceleoPreferences.switchQueryCache(false);
+      	AcceleoPreferences.switchDebugMessages(true);
+      	AcceleoPreferences.switchProfiler(true);
+      	AcceleoPreferences.switchSuccessNotifications(false);
+      } else
+      {
+      	AcceleoPreferences.switchDebugMessages(false);
+      	AcceleoPreferences.switchProfiler(false);
+      	AcceleoPreferences.switchSuccessNotifications(false);
+      }
+   }
+
+
+   /**
+    * Finds the output resource defined by the local {@link #outputFolderLocation} and the
+    * {@link #outputFileName}.
+    * 
+    * @return the output resource
+    */
+   private IResource findOutputResource()
+   {
+      // get workspace root
+		IWorkspaceRoot workSpaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+
+		// get target path within project
+		IPath targetFolderCont = new Path(outputFolderLocation);
+		IResource outputResource = null;
+		if (targetFolderCont.isAbsolute())
+		{
+			outputResource = workSpaceRoot.findMember(targetFolderCont);
+		} else
+		{
+			outputResource = workSpaceRoot.findMember(targetFolderCont.makeAbsolute());
+		}
+      return outputResource;
+   }
 }
